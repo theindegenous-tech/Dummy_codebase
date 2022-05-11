9d0247eba45958cbd426df98dfd25311c50bd25e
Sandeep <sandeep@theindegenous.org>
2022-05-11 16:02:02 +0530

    changes to folder structure:
    made changes to the folder structure, created the api folder which contains views, urls and serializers.
    
    changes to models:
    
    made changes to the relations from many to one to many to many.
    
    changes to the views:
    
    made changes to the views to show multiple countries,authors and publishers in the book-list

---
+++ backend/the_indegenous_backend/api/serializers.py
@@ -0,0 +1,54 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+from the_indegenous_backend.models import Book, Author, Publisher, Genre, Language, Country
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Language
+        fields = '__all__'
+
+class CountrySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Country
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Genre
+        fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    country = CountrySerializer(read_only=True)
+
+    class Meta:
+        model=Author
+        fields = '__all__'
+
+class PublisherSerializer1(serializers.ModelSerializer):
+
+    authors = AuthorSerializer(read_only=True, many=True)
+
+    class Meta:
+        model=Publisher
+        fields = '__all__'
+
+class PublisherSerializer2(serializers.ModelSerializer):
+    class Meta:
+        model=Publisher
+        fields=['id', 'title']
+
+class BookSerializer(serializers.ModelSerializer):
+    countries = CountrySerializer(read_only=True, many=True)
+    genre = GenreSerializer(read_only=True)
+    author = AuthorSerializer(read_only=True, many=True)
+    language = LanguageSerializer(read_only=True)
+    publisher = PublisherSerializer2(read_only=True, many=True)
+    sub_genre = GenreSerializer(read_only=True)
+    class Meta:
+        model=Book
+        fields = '__all__'
