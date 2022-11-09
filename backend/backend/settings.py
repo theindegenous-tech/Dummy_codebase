5c16118de92cd94f8905ddea46444c3f26d1f025
jagrit-Indegenous <jagrit@theindegenous.org>
2022-11-09 14:07:04 +0530

    cros issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,11 +111,11 @@ AUTH_PASSWORD_VALIDATORS = []
 
 
 #CORS setup
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
+    'http://localhost:3000','http://142.93.218.227:3000',"http://142.93.218.227:3001"
 )
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 
 # Internationalization
