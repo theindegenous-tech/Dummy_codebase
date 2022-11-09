2b7b1479d463b2dc61c189ae90490607fd134ae1
jagrit-Indegenous <jagrit@theindegenous.org>
2022-11-09 06:15:03 +0000

    deployment testing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-cn)99qrkgcut-nsx*4$43gbq68rj4-a-^1_)t4_jwv)x1z5i#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['142.93.218.227', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -84,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'library', 
-        'USER': 'postgres', 
+        'USER': 'admin', 
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
